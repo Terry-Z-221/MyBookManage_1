ec2910499e65195542eadd9734fd806e613cb9cf
zengmz0221 <974378437@qq>
2022-11-13 16:00:49 +0800

    主页面添加按钮页面跳转

--- app/src/main/java/cn/jnu/edu/mybookmanage/AddBookMessage.java
+++
@@ -1,4 +0,0 @@
-package cn.jnu.edu.mybookmanage;
-
-public class AddBookMessage {
-}
--- app/src/main/java/cn/jnu/edu/mybookmanage/MainActivity.java
+++ app/src/main/java/cn/jnu/edu/mybookmanage/MainActivity.java
@@ -2,6 +2,7 @@ package cn.jnu.edu.mybookmanage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button_add = findViewById(R.id.Button_Add);
-//
-//        button_add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        Button btn_add = (Button)findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AddBookActivity.class);
+                startActivity(i);
+            }
+        });
         //java匿名类
     }
 }
