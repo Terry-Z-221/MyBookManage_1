f2c78daf47f098880baf1b523756aa18ed610ca7
zengmz0221 <974378437@qq>
2022-11-20 13:02:15 +0800

    书籍信息添加页面制作-2

--- app/src/main/java/cn/jnu/edu/mybookmanage/MainActivity.java
+++ app/src/main/java/cn/jnu/edu/mybookmanage/MainActivity.java
@@ -2,28 +2,31 @@ package cn.jnu.edu.mybookmanage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_add = (Button)findViewById(R.id.btn_add);
+        Button btn_add = (Button)findViewById(R.id.btn_add); // 主页面添加书籍信息按钮
 
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,AddBookActivity.class);
-                startActivity(i);
-            }
+        btn_add.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this,AddBookActivity.class); // 跳转页面至添加信息页面
+            startActivity(i);
         });
         //java匿名类
     }
+
+
 }
